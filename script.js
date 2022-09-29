e471c8624cde65222d4dc938de2422fd1759dc44
LikaGreb <lika.greben@gmail.com>
2022-09-29 20:01:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let books = [
+    {
+        bookId: 1,
+        bookName: "JS part 1",
+        author: "Alen Fox",
+        pageQuant: 100,
+        pablisher: "NYPablisher",
+        copyOfBook: 2
+    }
+]
